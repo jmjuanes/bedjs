4f67de3ffa39fe1d773f2bfb1ce5c9592bd05fc5
jmjuanes <josemijuanes@gmail.com>
2016-06-12 01:47:30 +0200

    test/test.js: update test file.   - Created Test.Read function.   - Created Test.CollapseByRegion function.   - Created Test.CollapseByName function.   - Removed unused test file.

--- test/test.js
+++ test/test.js
@@ -1,19 +1,66 @@
 //Import bedJS
 var bedJS = require('../index.js');
 
-//Read the test file
-var bed = bedJS.Read('./test.bed');
+//Test function
+var Test =
+{
+	//Show bed file
+	ShowBED: function(bed)
+	{
+		//Read the full file
+		for(var i = 0; i < bed.length; i++)
+		{
+			//Show in console
+			console.log(JSON.stringify(bed[i]));
+		}
+	},
 
-//Show the bed content
-console.log('Bed in JSON:');
-console.log(bed);
+	//Test read a file
+	Read: function()
+	{
+		//Read the test file
+		var bed = bedJS.Read('./test.bed');
 
-//Group the bed content by name
-var group1 = bedJS.GroupByName(bed);
+		//Show the bed header
+		console.log('>> Bed in JSON:');
 
-//Show the group
-console.log('BED in groups: (1)');
-console.log(group1);
+		//Show the bed
+		Test.ShowBED(bed);
+	},
 
-//Save the group to a new file
-bedJS.Write('./group.bed', group1);
+	//Test collapse by name
+	CollapseByName: function()
+	{
+		//Read the test file
+		var bed = bedJS.Read('./test.bed');
+
+		//Collapse BED regions by name
+		var collapsed = bedJS.Collapse.ByName(bed);
+
+		//Show the collapsed regions
+		console.log('>> Collapse BED by region name');
+
+		//Show the bed
+		Test.ShowBED(collapsed);
+	},
+
+	//Test collapse by region
+	CollapseByRegion: function()
+	{
+		//Read the test file
+		var bed = bedJS.Read('./test.bed');
+
+		//Collapse BED regions by name
+		var collapsed = bedJS.Collapse.ByRegion(bed);
+
+		//Show the collapsed regions
+		console.log('>> Collapse BED by region');
+
+		//Show the bed
+		Test.ShowBED(collapsed);
+	}
+
+};
+
+//Exports to node
+module.exports = Test;
